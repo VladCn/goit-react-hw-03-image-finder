106cc0248c1dc8a70979950af8262303e1027bfd
VladZ <vladislav.zaytcev84@gmail.com>
2022-02-23 17:35:48 +0200

    add open and close modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-import * as basicLightbox from 'basiclightbox';
 import { Audio } from 'react-loader-spinner';
 import { Searchbar } from './Searchbar';
 import { ImageGallery } from './ImageGallery';
