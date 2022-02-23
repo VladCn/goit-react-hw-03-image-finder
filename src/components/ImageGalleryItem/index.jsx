e862d8851ce23ad619fdeba62ff9a64bc2982ee8
VladZ <vladislav.zaytcev84@gmail.com>
2022-02-23 17:44:24 +0200

    add open and close modal

--- src/components/ImageGalleryItem/index.jsx
+++ src/components/ImageGalleryItem/index.jsx
@@ -3,11 +3,7 @@ import React from 'react';
 export function ImageGalleryItem({ id, largeImageURL, webformatURL }) {
   return (
     <li className="gallery-item" id={id}>
-      <img
-        src={webformatURL}
-        alt="gallery image"
-        data-largeimage={largeImageURL}
-      />
+      <img src={webformatURL} alt="malunok" data-largeimage={largeImageURL} />
     </li>
   );
 }
